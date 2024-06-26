15c34b649eecedcc54e4baa86a7cd178ebb2f67c
AngelPeruFachero <149626423+AngelPeruFachero@users.noreply.github.com>
2024-06-25 21:41:12 -0500

    Actualizar config.js

--- config.js
+++ config.js
@@ -430,7 +430,7 @@ import(`${file}?update=${Date.now()}`);
 })
 
 async function fetchDataAndProcess() {
-const response = await fetch('https://raw.githubusercontent.com/KatashiFukushima/KatashiBot-MD/master/official_accounts.json')
+const response = await fetch('https://raw.githubusercontent.com/AngelPeruFachero/Broly-Bot-MD/master/official_accounts.json')
 const data = await response.json() 
 let { accounts, channels, groups, collaboration, sponsors, others } = data.info
 
