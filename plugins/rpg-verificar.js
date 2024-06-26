9a76dbf19c83f4bc0b325f3c7c00ea8ec1bad79b
AngelPeruFachero <149626423+AngelPeruFachero@users.noreply.github.com>
2024-06-25 20:15:42 -0500

    Actualizar rpg-verificar.js

--- plugins/rpg-verificar.js
+++ plugins/rpg-verificar.js
@@ -4,14 +4,14 @@ let handler = async function (m, { conn, text, usedPrefix, command }) {
 let user = global.db.data.users[m.sender]
 let name2 = conn.getName(m.sender)
 if (user.registered === true) throw `🫂 *Ya estas registrado, para volver a registrarte, usa el comando: #unreg*`
-if (!Reg.test(text)) throw `🥷 *El comando ingresado es incorrecto, uselo de la siguiente manera:*\n\n!reg nombre.edad\n\nEjemplo: !reg ${name2}.21`
+if (!Reg.test(text)) throw `🥷 *El comando ingresado es incorrecto, uselo de la siguiente manera:*\n\n!reg nombre.edad\n\nEjemplo: !reg ${name2}.18`
 let [_, name, splitter, age] = text.match(Reg)
 if (!name) throw '☘️ *No puedes registrarte sin nombre, el nombre es obligatorio. Inténtelo de nuevo.*'
 if (!age) throw '🪴 *No puedes registrarte sin la edad, la edad es opcional. Inténtelo de nuevo.*'
 if (name.length >= 30) throw '💫 *El nombre no debe de tener mas de 30 caracteres.*' 
 age = parseInt(age)
 if (age > 999) throw '*😏 Viejo/a Sabroso/a*'
-if (age < 5) throw '🥷 *Ven aquí, te adoptare!!*'
+if (age < 5) throw '🌟 *Ven aquí, te adoptare!!*'
 user.name = name.trim()
 user.age = age
 user.regTime = + new Date
@@ -22,14 +22,14 @@ global.db.data.users[m.sender].exp += 150
 global.db.data.users[m.sender].joincount += 5
 let sn = createHash('md5').update(m.sender).digest('hex').slice(0, 6)        
 //m.react('✅️') 
-let regbot = `🥷 *R E G I S T R A D O* 🥷
+let regbot = `🌟 *R E G I S T R A D O* 🌟
 •┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄•
 ✨️ Nombre: ${name}
 🧃 Edad: ${age} años
 •┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄•
 🎁 Recompensas:
 • 7 Diamantes 💎
-• 5 KatashiCoins 💰
+• 5 BrolyCoins 💰
 • 150 Experiencia 💸
 • 3 Tokens 🪙
 •┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄•`
