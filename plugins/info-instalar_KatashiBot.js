45a83fbab390af7398ad070daf4e9638543c5c32
AngelPeruFachero <149626423+AngelPeruFachero@users.noreply.github.com>
2024-07-11 06:37:06 -0500

    Actualizar info-instalar_BrolyBot.js
    𝗔𝗻𝗴𝗲𝗹𝗣𝗲𝗿𝘂𝗙𝗮𝗰𝗵𝗲𝗿𝗼

--- plugins/info-instalar_KatashiBot.js
+++ plugins/info-instalar_KatashiBot.js
@@ -1,11 +1,11 @@
 let handler  = async (m, { conn, usedPrefix, command }) => {
 let picture = './media/menus/Menu1.jpg'
-let gata = `𝑰𝑵𝑺𝑻𝑨𝑳𝑨𝑪𝑰Ó𝑵 𝑫𝑬 𝑲𝑨𝑻𝑨𝑺𝑯𝑰𝑩𝑶𝑻 🥷
+let gata = `𝑰𝑵𝑺𝑻𝑨𝑳𝑨𝑪𝑰Ó𝑵 𝑫𝑬 𝑩𝑹𝑶𝑳𝒀𝑩𝑶𝑻 💻
 
 *━━━━━━━━━━━━━⬣*
 ✅ 𝗜𝗡𝗦𝗧𝗔𝗟𝗔𝗥 𝗘𝗡 𝗜𝗡𝗙𝗜𝗡𝗜𝗧𝗬
 *⎯ ⎯ ⎯ ⎯ ⎯ ⎯ ⎯ ⎯ ⎯ ⎯ ⎯ ⎯ ⎯*
-*𝑲𝒂𝒕𝒂𝒔𝒉𝒊𝑩𝒐𝒕 : 𝑰𝒏𝒇𝒊𝒏𝒊𝒕𝒚*
+*𝔹𝕣𝕠𝕝𝕪𝔹𝕠𝕥 : 𝕀𝕟𝕗𝕚𝕟𝕚𝕥𝕪*
 *_Tutorial en proceso..._*
 *⎯ ⎯ ⎯ ⎯ ⎯ ⎯ ⎯ ⎯ ⎯ ⎯ ⎯ ⎯ ⎯*
 _DASHBOARD_
@@ -24,14 +24,14 @@ _https://store.panel-infinitywa.store_
 *𝑲𝒂𝒕𝒂𝒔𝒉𝒊𝑩𝒐𝒕 : 𝑰𝒏𝒔𝒕𝒂𝒍𝒂𝒄𝒊ó𝒏 𝑨𝒖𝒕𝒐𝒎á𝒕𝒊𝒄𝒂*
 *_https://www.youtube.com/shorts/ZLJYDUM6vSY_*
 termux-setup-storage
-apt update -y && yes | apt upgrade && pkg install -y bash wget mpv && wget -O - https://raw.githubusercontent.com/KatashiFukushima/KatashiBot-MD/master/katashi.sh | bash
+apt update -y && yes | apt upgrade && pkg install -y bash wget mpv && wget -O - https://raw.githubusercontent.com/AngelPeruFachero/Broly-Bot-MD/master/Broly.sh | bash
 *━━━━━━━━━━━━━⬣*`
 await 
-//conn.sendFile(m.chat, gataImg, 'lp.jpg', gata, fkontak, false, { contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, title: gt, body: ' 😻 𝗦𝘂𝗽𝗲𝗿 𝗚𝗮𝘁𝗮𝗕𝗼𝘁-𝗠𝗗 - 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽 ', previewType: 0, thumbnail: imagen4, sourceUrl: accountsgb }}})}
+//conn.sendFile(m.chat, gataImg, 'lp.jpg', gata, fkontak, false, { contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, title: gt, body: ' 🌟 𝗦𝘂𝗽𝗲𝗿 𝗕𝗿𝗼𝗹𝘆-𝗕𝗼𝘁-𝗠𝗗 - 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽 ', previewType: 0, thumbnail: imagen4, sourceUrl: accountsgb }}})}
 conn.sendButton(m.chat, gata, `Comunícate con Mi Creador si necesitas ayuda con la Instalación.\n\nContact My Creator if you need help with the Installation.\n\n${ig}\n${wm}`, picture, [
 ['𝘾𝙪𝙚𝙣𝙩𝙖𝙨 𝙊𝙛𝙞𝙘𝙞𝙖𝙡𝙚𝙨 | 𝘼𝙘𝙘𝙤𝙪𝙣𝙩𝙨 ✅', '.cuentaskb'],
-['🎁 𝘿𝙤𝙣𝙖𝙧 | 𝘿𝙤𝙣𝙖𝙩𝙚', '.donar']], 'termux-setup-storage\n\napt update -y && yes | apt upgrade && pkg install -y bash wget mpv && wget -O - https://raw.githubusercontent.com/KatashiFukushima/KatashiBot-MD/master/katashi.sh | bash', fkontak)}
-handler.command = /^(instalarbot|instalarkatashibot|instalarkatashi|procesobot|botproceso|procesodelbot|botinstall|installbot)/i
+['🎁 𝘿𝙤𝙣𝙖𝙧 | 𝘿𝙤𝙣𝙖𝙩𝙚', '.donar']], 'termux-setup-storage\n\napt update -y && yes | apt upgrade && pkg install -y bash wget mpv && wget -O - https://raw.githubusercontent.com/AngelPeruFachero/Broly-Bot-MD/master/Broly.sh | bash', fkontak)}
+handler.command = /^(instalarbot|instalarbrolybot|instalarbroly|procesobot|botproceso|procesodelbot|botinstall|installbot)/i
 export default handler
 
 
