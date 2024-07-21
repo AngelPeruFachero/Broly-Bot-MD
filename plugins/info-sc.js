c04425a94240a4c3ab97761983f5002ffe2516ac
AngelPeruFachero <149626423+AngelPeruFachero@users.noreply.github.com>
2024-07-20 19:10:16 -0500

    Actualizar info-sc.js

--- plugins/info-sc.js
+++ plugins/info-sc.js
@@ -21,7 +21,7 @@ function kyun(seconds){
 					const runtime = process.uptime()
 		            const teks = `${kyun(runtime)}`
 					const itsme = `0@s.whatsapp.net`
-					const split = `🥷 𝗦𝘂𝗽𝗲𝗿 𝗞𝗮𝘁𝗮𝘀𝗵𝗶𝗕𝗼𝘁-𝗠𝗗 - 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽`
+					const split = `💥 𝗦𝘂𝗽𝗲𝗿 𝗕𝗿𝗼𝗹𝘆 - 𝗕𝗼𝘁-𝗠𝗗 - 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽`
 					const rtimebro = {
 					contextInfo: {
 					participant: itsme,
