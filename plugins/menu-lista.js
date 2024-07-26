995a61bbb902cfb515e9b631a14d0d6be1c7add0
AngelPeruFachero <149626423+AngelPeruFachero@users.noreply.github.com>
2024-07-26 07:18:43 -0500

    Actualizar menu-lista.js

--- plugins/menu-lista.js
+++ plugins/menu-lista.js
@@ -164,7 +164,7 @@ name: "single_select",
 buttonParamsJson
 }]
 }}
-let katashi = `𝗘𝗟 𝗠𝗘𝗡𝗨 𝗦𝗢𝗟𝗜𝗖𝗜𝗧𝗔𝗗𝗢 𝗘𝗦 𝗩𝗜𝗦𝗜𝗕𝗟𝗘 𝗨𝗡𝗜𝗖𝗔𝗠𝗘𝗡𝗧𝗘 𝗣𝗔𝗥𝗔 𝗨𝗦𝗨𝗔𝗥𝗜𝗢𝗦 𝗖𝗢𝗡 𝗗𝗜𝗦𝗣𝗢𝗦𝗜𝗧𝗜𝗩𝗢𝗦 𝗔𝗡𝗗𝗥𝗢𝗜𝗗.`.trim()
+let katashi = `𝗘𝗡𝗩𝗜𝗔𝗡𝗗𝗢 𝗠𝗘𝗡𝗨 𝗗𝗘 𝗕𝗥𝗢𝗟𝗬 - 𝗕𝗢𝗧 - 𝗠𝗗 𝗘𝗦𝗣𝗘𝗥𝗘 𝗨𝗡 𝗠𝗢𝗠𝗘𝗡𝗧𝗢....`.trim()
 const message = { messageContextInfo: { deviceListMetadata: {}, deviceListMetadataVersion: 2 }, interactiveMessage }
 await conn.sendFile(m.chat, pp, 'katashi.mp4', katashi, fkontak, false, { contextInfo: {mentionedJid, externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, title: gt, body: ' 💥 𝗦𝘂𝗽𝗲𝗿 𝗕𝗿𝗼𝗹𝘆-𝗕𝗼𝘁-𝗠𝗗 - 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽 ', previewType: 0, thumbnail: imagen4, sourceUrl: redesMenu}}})
 await conn.relayMessage(m.chat, { viewOnceMessage: { message } }, {})
