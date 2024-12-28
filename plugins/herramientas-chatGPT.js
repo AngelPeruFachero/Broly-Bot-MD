a5e16b7ff2af699a2bf24280c6c60ceb770ffb5a
AngelPeruFachero <149626423+AngelPeruFachero@users.noreply.github.com>
2024-12-27 22:03:01 -0500

    Actualizar herramientas-chatGPT.js

--- plugins/herramientas-chatGPT.js
+++ plugins/herramientas-chatGPT.js
@@ -1,32 +1,72 @@
-import fetch from 'node-fetch';
-import axios from 'axios';
-import translate from '@vitalets/google-translate-api';
-import {Configuration, OpenAIApi} from 'openai';
-const configuration = new Configuration({organization: global.openai_org_id, apiKey: global.openai_key});
-const openaiii = new OpenAIApi(configuration);
-const handler = async (m, {conn, text, usedPrefix, command}) => {
-if (usedPrefix == 'a' || usedPrefix == 'A') return;
-if (!text) throw `*${lenguajeGB['smsAvisoMG']()}𝙄𝙉𝙂𝙍𝙀𝙎𝙀 𝙐𝙉𝘼 𝙋𝙀𝙏𝙄𝘾𝙄𝙊𝙉 𝙊 𝙐𝙉𝘼 𝙊𝙍𝘿𝙀𝙉 𝙋𝘼𝙍𝘼 𝙐𝙎𝘼𝙍 𝙇𝘼 𝙁𝙐𝙉𝘾𝙄𝙊𝙉 𝘿𝙀𝙇 𝘾𝙃𝘼𝙏𝙂𝙋𝙏\n\n❏ 𝙀𝙅𝙀𝙈𝙋𝙇𝙊 𝘿𝙀 𝙋𝙀𝙏𝙄𝘾𝙄𝙊𝙉𝙀𝙎 𝙔 𝙊𝙍𝘿𝙀𝙉𝙀𝙎\n❏ ${usedPrefix + command} Recomienda un top 10 de películas de acción\n❏ ${usedPrefix + command} Codigo en JS para un juego de cartas`    
+import axios from 'axios'
+import fetch from 'node-fetch'
 
-if (command == 'ia' || command == 'chatgpt') {
+let handler = async (m, { conn, usedPrefix, command, text }) => {
+const isQuotedImage = m.quoted && (m.quoted.msg || m.quoted).mimetype && (m.quoted.msg || m.quoted).mimetype.startsWith('image/')
+const username = `${conn.getName(m.sender)}`
+const basePrompt = `Tu nombre es BrolyBot y parece haber sido creado por BrayanCrazzy. Tú usas el idioma Español. Llamarás a las personas por su nombre ${username}, te gusta ser divertida, te encanta aprender y sobre todo las explociones. Lo más importante es que debes ser amigable con la persona con la que estás hablando. ${username}`
+if (isQuotedImage) {
+const q = m.quoted
+const img = await q.download?.()
+if (!img) {
+console.error('🚩 Error: No image buffer available')
+return conn.reply(m.chat, '🚩 Error: No se pudo descargar la imagen.', m, fake)}
+const content = '🚩 ¿Qué se observa en la imagen?'
 try {
-conn.sendPresenceUpdate('composing', m.chat);
-let gpt = await fetch(`https://deliriusapi-official.vercel.app/ia/gptweb?text=${text}`) 
-let res = await gpt.json()
-await m.reply(res.gpt)
-} catch {
+const imageAnalysis = await fetchImageBuffer(content, img)
+const query = '😊 Descríbeme la imagen y detalla por qué actúan así. También dime quién eres'
+const prompt = `${basePrompt}. La imagen que se analiza es: ${imageAnalysis.result}`
+const description = await luminsesi(query, username, prompt)
+await conn.reply(m.chat, description, m, fake)
+} catch (error) {
+console.error('🚩 Error al analizar la imagen:', error)
+await conn.reply(m.chat, '🚩 Error al analizar la imagen.', m, fake)}
+} else {
+if (!text) { return conn.reply(m.chat, `🍟 *Ingrese su petición*\n🚩 *Ejemplo de uso:* ${usedPrefix + command} Como hacer un avión de papel`, m, rcanal)}
+await m.react('💬')
 try {
-let gpt = await fetch(`https://deliriusapi-official.vercel.app/ia/chatgpt?q=${text}`)
-let res = await gpt.json()
-await m.reply(res.data)
-} catch {
-}}}
+const query = text
+const prompt = `${basePrompt}. Responde lo siguiente: ${query}`
+const response = await luminsesi(query, username, prompt)
+await conn.reply(m.chat, response, m, fake)
+} catch (error) {
+console.error('🚩 Error al obtener la respuesta:', error)
+await conn.reply(m.chat, 'Error: intenta más tarde.', m, fake)}}}
 
-if (command == 'openai' || command == 'ia2' || command == 'chatgpt2') {
-conn.sendPresenceUpdate('composing', m.chat);
-let gpt = await fetch(`https://deliriusapi-official.vercel.app/ia/gptweb?text=${text}`) 
-let res = await gpt.json()
-await m.reply(res.gpt)
-}}
-handler.command = /^(openai|chatgpt|ia|ai|openai2|chatgpt2|ia2)$/i;
-export default handler;
+handler.help = ['chatgpt <texto>', 'ia <texto>']
+handler.tags = ['ai']
+handler.group = true;
+handler.register = true
+
+// handler.estrellas = 1
+handler.command = ['ia', 'chatgpt']
+
+export default handler
+
+// Función para enviar una imagen y obtener el análisis
+async function fetchImageBuffer(content, imageBuffer) {
+try {
+const response = await axios.post('https://Luminai.my.id', {
+content: content,
+imageBuffer: imageBuffer 
+}, {
+headers: {
+'Content-Type': 'application/json' 
+}})
+return response.data
+} catch (error) {
+console.error('Error:', error)
+throw error }}
+// Función para interactuar con la IA usando prompts
+async function luminsesi(q, username, logic) {
+try {
+const response = await axios.post("https://Luminai.my.id", {
+content: q,
+user: username,
+prompt: logic,
+webSearchMode: false
+})
+return response.data.result
+} catch (error) {
+console.error('🚩 Error al obtener:', error)
+throw error }}
